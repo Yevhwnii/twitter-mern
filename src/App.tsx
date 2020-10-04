1506a074881d1fb9fa902975a5c5aff8460b62ec
Yevhwnii <mirandoos3223@gmail.com>
2020-10-04 20:27:24 +0200

    signin and home routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 
 function App() {
   return (
     <div className='App'>
-      <SignIn />
+      <Switch>
+        <Route path='/signin'>
+          <SignIn />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
