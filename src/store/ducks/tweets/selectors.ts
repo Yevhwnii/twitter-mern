a89930ef0c464856e367b4b3a25b6a4b511b8b9c
Yevhwnii <mirandoos3223@gmail.com>
2020-10-11 19:06:36 +0200

    selectors for accesing redux state in app

---
+++ src/store/ducks/tweets/selectors.ts
@@ -0,0 +1,13 @@
+import { createSelector } from "reselect";
+import { RootState } from "../../store";
+import { LoadingState, TweetsState } from "./contracts/state";
+
+const selectTweetState = (state: RootState) : TweetsState => state.tweets
+
+export const selectTweets = createSelector(selectTweetState, tweets => tweets.items)
+
+export const selectLoadingState = (state: RootState) => selectTweetState(state).loadingState
+
+export const selectIsLoadingTweetsState = (state: RootState) : boolean => selectLoadingState(state) === LoadingState.LOADING
+
+export const selectIsLoadedTweetsState = (state: RootState) : boolean => selectLoadingState(state) === LoadingState.LOADED
