42b5b52e0b1fe41a656cb5ae3a7744ed9b928dad
Yevhwnii <mirandoos3223@gmail.com>
2020-10-11 19:06:49 +0200

    enabling redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import './index.css';
 import App from './App';
 import theme from './theme';
+import { store } from './store/store';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </Router>
     </ThemeProvider>
   </React.StrictMode>,
