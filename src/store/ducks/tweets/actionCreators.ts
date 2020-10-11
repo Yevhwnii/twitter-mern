f2ca1c38cb5883d1acdd1acf8a966329a351ac4b
Yevhwnii <mirandoos3223@gmail.com>
2020-10-11 19:05:50 +0200

    actions which can be dispatched

---
+++ src/store/ducks/tweets/actionCreators.ts
@@ -0,0 +1,42 @@
+import { Action } from 'redux';
+import { LoadingState, TweetsState } from './contracts/state';
+
+// Name of action types 
+export enum TweetActionTypes {
+  SET_TWEETS = 'tweets/SET_TWEETS',
+  FETCH_TWEETS = 'tweets/FETCH_TWEETS',
+  SET_LOADING_STATE = 'tweets/SET_LOADING_STATE'
+}
+
+// Interface which regulates setTweets action
+export interface SetTweetsActionInterface extends Action<TweetActionTypes> {
+  type: TweetActionTypes.SET_TWEETS;
+  payload: TweetsState['items'];
+}
+
+export interface FetchTweetsActionInterface extends Action<TweetActionTypes> {
+  type: TweetActionTypes.FETCH_TWEETS;
+}
+
+export interface SetTweetsLoadingStateActionInterface extends Action<TweetActionTypes> {
+  type: TweetActionTypes.SET_LOADING_STATE;
+  payload: LoadingState;
+}
+
+// Function itself which is returning action object
+export const setTweets = (payload: TweetsState['items']): SetTweetsActionInterface => ({
+  type: TweetActionTypes.SET_TWEETS,
+  payload,
+});
+
+export const setTweetsLoadingState = (payload: LoadingState): SetTweetsLoadingStateActionInterface => ({
+  type: TweetActionTypes.SET_LOADING_STATE,
+  payload,
+});
+
+export const fetchTweets = (): FetchTweetsActionInterface => ({
+  type: TweetActionTypes.FETCH_TWEETS,
+});
+
+// Tweets action available in other files
+export type TweetsActions = SetTweetsActionInterface | SetTweetsLoadingStateActionInterface | FetchTweetsActionInterface
