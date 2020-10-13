30c1416d8eabd3d7424f60ea9eb81165d1a0b79f
Yevhwnii <mirandoos3223@gmail.com>
2020-10-13 15:17:19 +0200

    changing endpoint to json-server db

--- src/services/api/tweetApi.ts
+++ src/services/api/tweetApi.ts
@@ -5,9 +5,7 @@ import { TweetsState } from "../../store/ducks/tweets/contracts/state"
 
 export const TweetsApi = {
     fetchTweets: async () : Promise<TweetsState['items']> => {
-        const response =  await axios.get('https://trycode.pw/c/2OBQ1.json')
+        const response =  await axios.get('/tweets')
         return response.data
-        
-        
     }
 }
