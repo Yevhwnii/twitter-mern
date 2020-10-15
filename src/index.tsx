3696a51717e02b31ab08f66579225058400a7568
Yevhwnii <mirandoos3223@gmail.com>
2020-10-15 23:58:55 +0200

    inject mui styles first in head

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import { StylesProvider, ThemeProvider } from '@material-ui/core/styles';
 
 import './index.css';
 import App from './App';
 import theme from './theme';
 import { store } from './store/store';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline />
       <Router>
         <Provider store={store}>
-          <App />
+          <StylesProvider injectFirst>
+            <App />
+          </StylesProvider>
         </Provider>
       </Router>
     </ThemeProvider>
