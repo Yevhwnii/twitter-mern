2c50e6d93f6d1734045c654004f9908e4960f2d3
Yevhwnii <mirandoos3223@gmail.com>
2020-10-16 00:01:11 +0200

    outsourcing page header with its logic

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,67 +6,74 @@ import { useHomeStyles } from './theme';
 import Tweet from '../../components/Tweet';
 import AddTweetForm from '../../components/AddTweetForm';
 import SideMenu from '../../components/SideMenu';
+import RightBar from '../../components/RightBar';
 import { fetchTweets } from '../../store/ducks/tweets/actionCreators';
 import {
   selectIsLoadingTweetsState,
   selectTweets,
 } from '../../store/ducks/tweets/selectors';
+import { fetchTopics } from '../../store/ducks/topics/actionCreators';
 
 // Material UI
 import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container/Container';
-import Paper from '@material-ui/core/Paper/Paper';
-import Typography from '@material-ui/core/Typography';
-import { StylesProvider } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
 import { CircularProgress } from '@material-ui/core';
-import RightBar from '../../components/RightBar';
+import { Route } from 'react-router-dom';
+import PageHeader from '../../components/PageHeader';
 
 const Home = () => {
   const classes = useHomeStyles();
   const dispatch = useDispatch();
   const tweets = useSelector(selectTweets);
-  const isLoading = useSelector(selectIsLoadingTweetsState);
+  const isTweetsLoading = useSelector(selectIsLoadingTweetsState);
 
   useEffect(() => {
     dispatch(fetchTweets());
+    dispatch(fetchTopics());
   }, [dispatch]);
 
   return (
-    <StylesProvider injectFirst>
-      <Container
-        className={classes.container}
-        style={{ height: '100vh', maxWidth: '1400px' }}
-        maxWidth='lg'>
-        <Grid container spacing={4}>
-          <Grid item xs={1} md={3}>
-            <SideMenu />
-          </Grid>
-          <Grid item xs={8} md={6}>
-            <Paper square className={classes.tweetsWrapper} variant='outlined'>
-              <Paper className='header' square variant='outlined'>
-                <Typography variant='h6'>Home page</Typography>
-              </Paper>
+    <Container
+      className={classes.container}
+      style={{ height: '100vh', maxWidth: '1400px' }}
+      maxWidth='lg'>
+      <Grid style={{ height: '100%' }} container spacing={4}>
+        <Grid item xs={1} md={3}>
+          <SideMenu />
+        </Grid>
+        <Grid item xs={8} md={6}>
+          <Paper square className={classes.tweetsWrapper} variant='outlined'>
+            <PageHeader />
+            <Route path={['/home', '/home/search']} exact>
               <Paper>
                 <AddTweetForm />
                 <div className={classes.addTweetForm__footer__line} />
               </Paper>
-              {isLoading ? (
+            </Route>
+            <Route path='/home' exact>
+              {isTweetsLoading ? (
                 <div style={{ textAlign: 'center', marginTop: 50 }}>
                   <CircularProgress />
                 </div>
               ) : (
                 tweets.map((tweet) => (
-                  <Tweet key={tweet._id} body={tweet.text} user={tweet.user} />
+                  <Tweet
+                    _id={tweet._id}
+                    key={tweet._id}
+                    body={tweet.text}
+                    user={tweet.user}
+                  />
                 ))
               )}
-            </Paper>
-          </Grid>
-          <Grid item xs={3} md={3}>
-            <RightBar />
-          </Grid>
+            </Route>
+          </Paper>
         </Grid>
-      </Container>
-    </StylesProvider>
+        <Grid item xs={3} md={3}>
+          <RightBar />
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
