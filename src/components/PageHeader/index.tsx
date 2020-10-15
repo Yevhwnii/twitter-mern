2c5526396f2eb9fecfaaed35fd687efa4ae8f9e9
Yevhwnii <mirandoos3223@gmail.com>
2020-10-15 23:59:10 +0200

    page header with its logic outsourced

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,76 @@
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import React, { useEffect, useState } from 'react';
+import { Redirect, useHistory, useLocation } from 'react-router-dom';
+
+import ArrowIcon from '@material-ui/icons/ArrowBack';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core';
+
+const usePageHeaderStyles = makeStyles(() => ({
+  modifiedHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    '& button': {
+      marginRight: 20,
+    },
+  },
+}));
+
+const PageHeader: React.FC = () => {
+  const location = useLocation();
+  const history = useHistory();
+  const [header, setHeader] = useState('');
+  const [showArrow, setShowArrow] = useState(false);
+  const classes = usePageHeaderStyles();
+
+  const queryParam = new URLSearchParams(location.search).get('q');
+  const tweetRegEx = /\/home\/tweet\/[A-Z,a-z,0-9]*/;
+
+  useEffect(() => {
+    switch (location.pathname + location.search) {
+      case '/home':
+        setHeader('Home page');
+        setShowArrow(false);
+        break;
+      case `/home/search?q=${queryParam}`:
+        setHeader('Topics');
+        setShowArrow(true);
+        break;
+      default:
+        if (tweetRegEx.test(location.pathname)) {
+          setHeader('Tweet');
+          setShowArrow(true);
+        }
+        break;
+    }
+  }, [location]);
+
+  const handleButton = () => {
+    history.goBack();
+  };
+
+  if (showArrow) {
+    return (
+      <Paper
+        style={{ padding: '1px 15px' }}
+        className='header'
+        square
+        variant='outlined'>
+        <div className={classes.modifiedHeader}>
+          <IconButton onClick={handleButton} color='primary'>
+            <ArrowIcon color='primary' />
+          </IconButton>
+          <Typography variant='h6'>{header}</Typography>
+        </div>
+      </Paper>
+    );
+  }
+  return (
+    <Paper className='header' square variant='outlined'>
+      <Typography variant='h6'>{header}</Typography>
+    </Paper>
+  );
+};
+
+export default PageHeader;
