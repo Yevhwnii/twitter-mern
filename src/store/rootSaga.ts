ca5a9d4946a257c67916889e6576cd6a8be5f0aa
Yevhwnii <mirandoos3223@gmail.com>
2020-10-20 18:01:26 +0200

    use single tweet saga

--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
+import { singleTweetSaga } from './ducks/singleTweet/sagas';
 import { topicsSaga } from './ducks/topics/sagas';
 import { tweetsSaga } from './ducks/tweets/sagas';
 
 export function* rootSaga() {
-  yield all([tweetsSaga(), topicsSaga()]);
+  yield all([tweetsSaga(), topicsSaga(), singleTweetSaga()]);
 }
