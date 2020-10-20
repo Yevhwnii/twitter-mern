a423747b2b878e3b503a50c78f9d1b0a0bce8836
Yevhwnii <mirandoos3223@gmail.com>
2020-10-20 18:01:01 +0200

    functionality for fetching single tweet

--- src/services/api/tweetApi.ts
+++ src/services/api/tweetApi.ts
@@ -1,11 +1,14 @@
-
-import axios from 'axios'
-import { TweetsState } from "../../store/ducks/tweets/contracts/state"
-
+import axios from 'axios';
+import { SingleTweetState } from '../../store/ducks/singleTweet/contracts/state';
+import { TweetsState } from '../../store/ducks/tweets/contracts/state';
 
 export const TweetsApi = {
-    fetchTweets: async () : Promise<TweetsState['items']> => {
-        const response =  await axios.get('/tweets')
-        return response.data
-    }
-}
+  fetchTweets: async (): Promise<TweetsState['items']> => {
+    const response = await axios.get('/tweets');
+    return response.data;
+  },
+  fetchSingleTweet: async (id: string): Promise<SingleTweetState['data']> => {
+    const response = await axios.get(`/tweets?_id=${id}`);
+    return response.data[0];
+  },
+};
