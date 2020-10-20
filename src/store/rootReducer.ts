4a28382deac590e2e776f01f1b7def4416ee983a
Yevhwnii <mirandoos3223@gmail.com>
2020-10-20 18:01:17 +0200

    use single tweet reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { singleTweetReducer } from './ducks/singleTweet/reducer';
 import { topicsReducer } from './ducks/topics/reducer';
 import { tweetsReducer } from './ducks/tweets/reducer';
 
 export const rootReducer = combineReducers({
   tweets: tweetsReducer,
   topics: topicsReducer,
+  singleTweet: singleTweetReducer,
 });
