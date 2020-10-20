8c98677c1abdd849f94b79cc474aa7ec5d3d0b9f
Yevhwnii <mirandoos3223@gmail.com>
2020-10-20 18:00:31 +0200

    single tweet component with buffer cleaning

---
+++ src/components/Tweet/SingleTweet/index.tsx
@@ -0,0 +1,44 @@
+import CircularProgress from '@material-ui/core/CircularProgress';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import Tweet from '..';
+import {
+  clearTweetBuffer,
+  fetchTweetData,
+} from '../../../store/ducks/singleTweet/actionCreators';
+import {
+  selectIsLoadingSingleTweetState,
+  selectSingleTweet,
+} from '../../../store/ducks/singleTweet/selectors';
+
+const SingleTweet: React.FC = () => {
+  const dispatch = useDispatch();
+  let singleTweet = useSelector(selectSingleTweet);
+  const isLoading = useSelector(selectIsLoadingSingleTweetState);
+  const params: { id: string } = useParams();
+  const id = params.id;
+
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchTweetData(id));
+    }
+    return () => {
+      dispatch(clearTweetBuffer());
+    };
+  }, [dispatch, id]);
+
+  if (isLoading) {
+    return (
+      <div style={{ textAlign: 'center', marginTop: 50 }}>
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  return (
+    <>{singleTweet && <Tweet body={singleTweet.text} {...singleTweet} />}</>
+  );
+};
+
+export default SingleTweet;
