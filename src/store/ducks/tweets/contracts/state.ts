8592c81f17308b088d82cb1818ae8e098f91e854
Yevhwnii <mirandoos3223@gmail.com>
2020-10-22 18:04:37 +0200

    AddForm status enum

--- src/store/ducks/tweets/contracts/state.ts
+++ src/store/ducks/tweets/contracts/state.ts
@@ -1,5 +1,12 @@
 import { LoadingState } from '../../common/contracts/loadingState';
 
+export enum AddFormStatus {
+  LOADING = 'LOADING',
+  ERROR = 'ERROR',
+  NEVER = 'NEVER',
+  FINISHED = 'FINISHED',
+}
+
 export interface Tweet {
   _id: string;
   text: string;
@@ -13,4 +20,5 @@ export interface Tweet {
 export interface TweetsState {
   items: Tweet[];
   loadingState: LoadingState;
+  addTweetStatus: AddFormStatus;
 }
