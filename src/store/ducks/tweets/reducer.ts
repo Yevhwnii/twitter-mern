a04054ec2379f4182d8640b5b725c65c69178c35
Yevhwnii <mirandoos3223@gmail.com>
2020-10-22 18:03:50 +0200

    AddForm status reducer

--- src/store/ducks/tweets/reducer.ts
+++ src/store/ducks/tweets/reducer.ts
@@ -1,10 +1,11 @@
 import produce, { Draft } from 'immer';
 import { LoadingState } from '../common/contracts/loadingState';
-import { TweetActionTypes, TweetsActions } from './actionCreators';
-import { TweetsState } from './contracts/state';
+import { TweetActionTypes, TweetsActions } from './contracts/actionTypes';
+import { AddFormStatus, TweetsState } from './contracts/state';
 
 const initialTweetsState: TweetsState = {
   items: [],
+  addTweetStatus: AddFormStatus.NEVER,
   loadingState: LoadingState.NEVER,
 };
 
@@ -19,8 +20,18 @@ export const tweetsReducer = produce(
         draft.items = [];
         draft.loadingState = action.payload;
         break;
+      case TweetActionTypes.SET_ADDFORM_STATUS:
+        draft.addTweetStatus = action.payload;
+        break;
       case TweetActionTypes.FETCH_TWEETS:
         draft.loadingState = LoadingState.LOADING;
+        break;
+      case TweetActionTypes.ADD_TWEET:
+        draft.items.unshift(action.payload);
+        draft.addTweetStatus = AddFormStatus.FINISHED;
+        break;
+      case TweetActionTypes.ASYNC_ADD_TWEET:
+        draft.addTweetStatus = AddFormStatus.LOADING;
         break;
       default:
         break;
