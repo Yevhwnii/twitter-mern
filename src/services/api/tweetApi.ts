0e1dd1e9718261d34c8068d0e824345402e9fd2c
Yevhwnii <mirandoos3223@gmail.com>
2020-10-22 18:03:06 +0200

    add tweet request

--- src/services/api/tweetApi.ts
+++ src/services/api/tweetApi.ts
@@ -1,14 +1,18 @@
 import axios from 'axios';
 import { SingleTweetState } from '../../store/ducks/singleTweet/contracts/state';
-import { TweetsState } from '../../store/ducks/tweets/contracts/state';
+import { Tweet, TweetsState } from '../../store/ducks/tweets/contracts/state';
 
 export const TweetsApi = {
   fetchTweets: async (): Promise<TweetsState['items']> => {
-    const response = await axios.get('/tweets');
+    const response = await axios.get('/tweets?_sort=id&_order=desc');
     return response.data;
   },
   fetchSingleTweet: async (id: string): Promise<SingleTweetState['data']> => {
     const response = await axios.get(`/tweets?_id=${id}`);
     return response.data[0];
+  },
+  addTweet: async (tweet: Tweet): Promise<Tweet> => {
+    const response = await axios.post('/tweets', tweet);
+    return response.data;
   },
 };
