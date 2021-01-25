201a3fa3827872c546f2c1c064c8944c74dcb03f
Yevhwnii <mirandoos3223@gmail.com>
2021-01-25 17:05:36 +0100

    update api due to needs of backend

--- src/services/api/tweetApi.ts
+++ src/services/api/tweetApi.ts
@@ -1,18 +1,18 @@
-import axios from 'axios';
+import { axios } from '../../core/axios';
 import { SingleTweetState } from '../../store/ducks/singleTweet/contracts/state';
 import { Tweet, TweetsState } from '../../store/ducks/tweets/contracts/state';
 
 export const TweetsApi = {
   fetchTweets: async (): Promise<TweetsState['items']> => {
-    const response = await axios.get('/tweets?_sort=id&_order=desc');
+    const response = await axios.get('/tweets');
     return response.data;
   },
   fetchSingleTweet: async (id: string): Promise<SingleTweetState['data']> => {
-    const response = await axios.get(`/tweets?_id=${id}`);
-    return response.data[0];
+    const response = await axios.get(`/tweets/${id}`);
+    return response.data;
   },
-  addTweet: async (tweet: Tweet): Promise<Tweet> => {
-    const response = await axios.post('/tweets', tweet);
+  addTweet: async (payload: string): Promise<Tweet> => {
+    const response = await axios.post('/tweets', { text: payload });
     return response.data;
   },
 };
